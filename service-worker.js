d154f74b2195fd0305c1b8b049edead923420ab1
nonia16 <nonia16@freeuni.edu.ge>
2021-07-14 21:53:31 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f4f07b2786f411c6e4864efe001bdc8.js"
+  "./precache-manifest.0954ed4b246c1c6379d10b2aef693fd3.js"
 );
 
 self.addEventListener('message', (event) => {
